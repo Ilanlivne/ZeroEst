11e866ba9ba30678c6a174af79cd8b9cc7b426b4
Ilanlivne <ilan.livne@gmail.com>
2021-02-15 17:14:37 +0200

    adding naive function

---
+++ R/Naive.R
@@ -0,0 +1,11 @@
+
+
+naive <- function(X, Y) {
+  W = as.data.frame(X)*Y
+  mean_squared_W <- colMeans(W^2)              #calculate first element of beta_square_hat
+  var_W <- sapply(W,var)                       #calculate the second element of beta
+  beta_square_hat <- mean_squared_W - var_W    #calculate vector of beta2_hat
+  return(sum(beta_square_hat))
+
+}
+
